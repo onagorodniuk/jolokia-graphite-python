b86e9d4c84c560c38c71fbc4e7b852ececcc3b5c
onagorodniuk <gagarinq9@gmail.com>
2016-04-14 16:39:07 +0300

    replca comand line parameter with config file parameters

--- jolokia-graphite.py
+++ jolokia-graphite.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python
 import argparse
 import json
@@ -9,29 +10,35 @@ import socket
 import time
 import platform
 
+import os
+import ConfigParser
+
 #__author__ = 'onagorodniuk'
 parser = argparse.ArgumentParser(description='This is a demo script by nixCraft.')
-parser.add_argument('-m','--memory', help='java.lang:type=Memory/HeapMemoryUsage',required=True)
-parser.add_argument('-g','--gcyounggen',help='java.lang:name=G1 Young Generation,type=GarbageCollector', required=False)
-parser.add_argument('-o','--gcoldgen',help='java.lang:name=G1 Old Generation,type=GarbageCollector', required=False)
-parser.add_argument('-t','--threadscount',help='java.lang:type=Threading', required=False)
-parser.add_argument('-f','--graphite',help='graphite.dev-i.net, carbon port hardcoded inside script', required=True)
-parser.add_argument('-p','--prefix',help='server group prefix (project)', required=True)
-parser.add_argument('-j','--jolokiaurl',help='jolokia url, default value http://localhost:8080/jolokia', required=False)
-parser.add_argument('-v','--verbose', action="count", help='print outgoing mesaages to stdout', required=False)
+parser.add_argument('-c','--config',help='located /root/bin/jolokia-graphite.conf', required=True)
 args = parser.parse_args()
 
-if args.jolokiaurl != None:
-        BASE_URL = args.jolokiaurl
+config = ConfigParser.ConfigParser()
+config.read('/root/bin/jolokia-graphite.conf')
+memory = config.get('main', 'memory')
+gcyounggen = config.get('main', 'gcyounggen')
+gcoldgen = config.get('main', 'gcoldgen')
+threadscount = config.get('main', 'threadscount')
+graphite = config.get('main', 'graphite')
+prefix = config.get('main', 'prefix')
+jolokiaurl = config.get('main', 'jolokiaurl')
+verbose = config.get('main', 'verbose')
+
+
+if jolokiaurl != None:
+        BASE_URL = jolokiaurl
 else:
         BASE_URL = "http://localhost:8080/jolokia"
 ###DEFINE STANDART VALUES
 #short hostname
-node = platform.node()
-node = re.match(r"[a-zA-Z0-9\-]*", node)
-node = node.group(0)
+node = platform.node().replace('.', '-')
 timestamp = int(time.time())
-args.memory = 'java.lang:type=Memory/HeapMemoryUsage'
+#memory = 'java.lang:type=Memory/HeapMemoryUsage'
 #args.gcyounggen = 'java.lang:name=G1 Young Generation,type=GarbageCollector'
 #args.threadscount = 'java.lang:type=Threading'
 #args.gcoldgen = 'java.lang:name=G1 Old Generation,type=GarbageCollector'
@@ -40,21 +47,21 @@ GRAPHITE_PORT = 2003
 READ_URL = BASE_URL + "/?ignoreErrors=true&p=read/%s"
 
 def heap_usage(READ_URL):
-        url =  READ_URL % urllib.quote(args.memory)
+        url =  READ_URL % urllib.quote(memory)
         heap_data = json.load(urllib2.urlopen(url, timeout=5))
         heap_usage.max =  heap_data['value']['max']
         heap_usage.used = heap_data['value']['used']
 #hi()
 #print hi.bye
 def gc_young_gen(READ_URL):
-        url =  READ_URL % urllib.quote(args.gcyounggen)
+        url =  READ_URL % urllib.quote(gcyounggen)
         gc_data = json.load(urllib2.urlopen(url, timeout=5))
 #        print gc_data
         gc_young_gen.duration =  gc_data['value']['LastGcInfo']['duration']
 #        heap_usage.used = heap_data['value']['used']
 
 def gc_old_gen(READ_URL):
-        url =  READ_URL % urllib.quote(args.gcoldgen)
+        url =  READ_URL % urllib.quote(gcoldgen)
         gc_data = json.load(urllib2.urlopen(url, timeout=5))
 #        print gc_data
         gc_old_gen.duration =  gc_data['value']['LastGcInfo']['duration']
@@ -63,52 +70,52 @@ def gc_old_gen(READ_URL):
 
 
 def threads_count(READ_URL):
-        url =  READ_URL % urllib.quote(args.threadscount)
+        url =  READ_URL % urllib.quote(threadscount)
         threads_data = json.load(urllib2.urlopen(url, timeout=5))
 #        print gc_data
         threads_count.count =  threads_data['value']['ThreadCount']
 #        heap_usage.used = heap_data['value']['used']
 
 def send_msg(message):
-    if args.verbose:
+    if verbose == 'True':
             print 'sending message:\n%s' % message
     sock = socket.socket()
     sock.settimeout(2)
-    sock.connect((args.graphite, GRAPHITE_PORT))
+    sock.connect((graphite, GRAPHITE_PORT))
     sock.sendall(message)
     sock.close()
 
 ###SEND DATA TO GRAPHITE
-if args.memory != None:
+if memory != None:
         message = ''
         heap_usage(READ_URL)
         lines = [
-                '%s.%s.jmx.java.lang.type_Memory.HeapMemoryUsage.max %s %d' % (args.prefix, node, heap_usage.max, timestamp),
-                '%s.%s.jmx.java.lang.type_Memory.HeapMemoryUsage.used %s %d' % (args.prefix, node, heap_usage.used, timestamp)]
+                '%s.%s.jmx.java.lang.type_Memory.HeapMemoryUsage.max %s %d' % (prefix, node, heap_usage.max, timestamp),
+                '%s.%s.jmx.java.lang.type_Memory.HeapMemoryUsage.used %s %d' % (prefix, node, heap_usage.used, timestamp)]
         message = '\n'.join(lines) + '\n'
         send_msg(message)
 #       send_msg(heap_usage.max)
 else:
         print 'memory Mbean not defined'
 
-if args.gcyounggen != None:
+if gcyounggen != None:
         message = ''
         try:
                 gc_young_gen(READ_URL)
                 lines = [
-                '%s.%s.jmx.java.lang.name_G1_Young_Generation.type_GarbageCollector.LastGcInfo.duration %s %d' % (args.prefix, node, gc_young_gen.duration, timestamp)]
+                '%s.%s.jmx.java.lang.name_G1_Young_Generation.type_GarbageCollector.LastGcInfo.duration %s %d' % (prefix, node, gc_young_gen.duration, timestamp)]
                 message = '\n'.join(lines) + '\n'
                 send_msg(message)
         except:
                 pass
 else:
         print 'GC Young Gen Mbean not defined'
-if args.gcoldgen != None:
+if gcoldgen != None:
         try:
                 message = ''
                 gc_old_gen(READ_URL)
                 lines = [
-                '%s.%s.jmx.java.lang.name_G1_Old_Generation.type_GarbageCollector.LastGcInfo.duration %s %d' % (args.prefix, node, gc_old_gen.duration, timestamp)]
+                '%s.%s.jmx.java.lang.name_G1_Old_Generation.type_GarbageCollector.LastGcInfo.duration %s %d' % (prefix, node, gc_old_gen.duration, timestamp)]
                 message = '\n'.join(lines) + '\n'
                 send_msg(message)
         except:
@@ -116,12 +123,12 @@ if args.gcoldgen != None:
 else:
         print 'GC Old Gen Mbean not defined'
 
-if args.threadscount != None:
+if threadscount != None:
         try:
                 message = ''
                 threads_count(READ_URL)
                 lines = [
-                '%s.%s.jmx.java.lang.type_Threading.ThreadCount %s %d' % (args.prefix, node, threads_count.count, timestamp)]
+                '%s.%s.jmx.java.lang.type_Threading.ThreadCount %s %d' % (prefix, node, threads_count.count, timestamp)]
                 message = '\n'.join(lines) + '\n'
                 send_msg(message)
         except:
